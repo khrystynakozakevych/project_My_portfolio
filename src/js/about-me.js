cf584a5f562212ef4c2f3c3aa9f8ea26abce7378
Caviar <forworker137@gmail.com>
2024-12-22 23:51:16 +0200

    added accoredion block for about-me section

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,2 +1,11 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+
+new Accordion('.accordion-block', {
+  showMultiple: true,
+  openOnInit: [0],
+});
+
+window.addEventListener('load', () => {
+  document.querySelector('.accordion-block').classList.remove('no-animation');
+});
