b68b55559f3030f2166165cefe77d19c33eeb514
Oleh Tatarynov <exocriador@gmail.com>
2024-12-25 21:05:22 +0200

    update

--- src/js/projects.js
+++ src/js/projects.js
@@ -2,6 +2,10 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 const swiper = new Swiper('.swiper', {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
     slidesPerView: 1,
     navigation: {
       nextEl: '.arrow-next',
