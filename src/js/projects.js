472c9c5ccefedfa8def73fa99d5a62ef72fcee98
Oleh Tatarynov <exocriador@gmail.com>
2024-12-25 16:19:12 +0200

    add tablet and desktop styles, fix js

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,21 +3,10 @@ import 'swiper/css/bundle';
 
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
-    spaceBetween: 16,
     navigation: {
       nextEl: '.arrow-next',
       prevEl: '.arrow-prev',
     },
-    breakpoints: {
-      768: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      1024: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-  },
     grabCursor: true,
     simulateTouch: true, 
   });
