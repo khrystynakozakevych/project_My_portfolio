6ae59c165100b1f32a074ed56c87da20bc4c1ba3
khrystyna_kozakevych <margaret.gray.777@gmail.com>
2024-12-26 22:19:57 +0100

    fix header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,23 +1,36 @@
-// Отримуємо елементи з DOM
-const menuButton = document.querySelector('.menu-button'); // Кнопка відкриття меню
-const closeMenuButton = document.querySelector('.close-menu'); // Кнопка закриття меню
-const mobileMenu = document.querySelector('.mobile-menu'); // Елемент мобільного меню
+// Mobile menu
+const menuButton = document.querySelector('.menu-button');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeMenuButton = document.querySelector('.close-menu');
+const mobileLinks = document.querySelectorAll('.mobile-menu a');
 
-// Функція для відкриття мобільного меню
+// Toggle for mobile menu
 menuButton.addEventListener('click', () => {
-    mobileMenu.classList.add('active'); // Додаємо клас, щоб зробити меню видимим
+  mobileMenu.classList.add('active');
 });
 
-// Функція для закриття мобільного меню
 closeMenuButton.addEventListener('click', () => {
-    mobileMenu.classList.remove('active'); // Видаляємо клас, щоб приховати меню
+  mobileMenu.classList.remove('active');
 });
-document.addEventListener('DOMContentLoaded', function () {
-    const menuButton = document.querySelector('.menu-text'); // Кнопка "Menu"
-    const tabletMenu = document.querySelector('.tablet-menu'); // Меню для планшета
 
-    // Додаємо обробник події на кнопку "Menu"
-    menuButton.addEventListener('click', function () {
-        tabletMenu.classList.toggle('active'); // Перемикаємо клас active для меню
-    });
+mobileLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+  });
+});
+
+// Tablet menu
+const tabletMenuButton = document.querySelector('.tablet-menu');
+const navTabletMenu = document.querySelector('.nav-tablet');
+const tabletLinks = document.querySelectorAll('.nav-tablet a, .order-button');
+
+// Toggle for tablet menu
+tabletMenuButton.addEventListener('click', () => {
+  navTabletMenu.classList.toggle('active');
+});
+
+tabletLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navTabletMenu.classList.remove('active');
+  });
 });
