ddaefe15ebca1a4402df638927d23e54107c227a
khrystyna_kozakevych <margaret.gray.777@gmail.com>
2024-12-27 17:12:22 +0100

    fix

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,12 +1,94 @@
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
+// import iziToast from "izitoast";
+// import "izitoast/dist/css/iziToast.min.css";
+
+// const slideContainer = document.querySelector(".container");
+// const reviemList = document.querySelector('.reviews-list');
+// const notFoundLabel = document.querySelector('.not-found');
+
+// function countSlidesPerView() {
+//   const containerWidth = slideContainer.clientWidth;
+//   if (containerWidth === 320) {
+//     return 1;
+//   } else if (containerWidth === 375) {
+//     return 1;
+//   } else if (containerWidth === 768) {
+//     return 2;
+//   } else if (containerWidth === 1440) {
+//     return 4;
+//   }
+// }
+// const swiper2 = new Swiper('.swiper2', {
+//   direction: 'horizontal',
+//   slidesPerView: countSlidesPerView(),
+//   spaceBetween: 16,
+//   slidesPerView: 1,
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//     hide: true,
+//   },
+//   navigation: {
+//     nextEl: '.right-btn2',
+//     prevEl: '.left-btn2',
+//   },
+//   speed: 750,
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: true,
+
+//   },
+//   grabCursor: true,
+//   simulateTouch: true,
+// });
+
+// function handleResize() {
+//   swiper2.params.slidesPerView = countSlidesPerView();
+//   swiper2.update();
+// }
+
+// window.addEventListener('resize', handleResize);
+
+// async function fetchReviews() {
+//   try {
+//     const response = await fetch("https://portfolio-js.b.goit.study/api/reviews");
+//     if (!response.ok) {
+//       throw new Error(`Error ${response.status}`);
+//     }
+//     const data = await response.json();
+//     renderReviews(data);
+//     notFoundLabel.classList.add('visually-hidden');
+//   } catch (error) {
+//     notFoundLabel.classList.remove('visually-hidden');
+//     iziToast.error({
+//       title: 'Error',
+//       message: 'Failed to fetch reviews from server',
+//       position: 'bottomRight',
+//     });
+//   }
+// }
+
+// const renderReviews = (data) => {
+//   data.forEach(element => {
+//     const markup = `<li class="reviews-item swiper-slide">
+//       <img class="img-reviews" src="${element.avatar_url}" alt="reviews" loading="lazy" width="48" height="48"/>
+//       <h3 class="reviews-head">${element.author}</h3>
+//       <p class="reviews-text">${element.review}</p>
+//     </li>`;
+//     reviemList.insertAdjacentHTML('beforeend', markup);
+//   });
+// }
+
+// fetchReviews();
+
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-const slideContainer = document.querySelector(".container");
+const slideContainer = document.querySelector('.container');
 const reviemList = document.querySelector('.reviews-list');
 const notFoundLabel = document.querySelector('.not-found');
-
 
 function countSlidesPerView() {
   const containerWidth = slideContainer.clientWidth;
@@ -24,7 +106,6 @@ const swiper2 = new Swiper('.swiper2', {
   direction: 'horizontal',
   slidesPerView: countSlidesPerView(),
   spaceBetween: 16,
-  slidesPerView: 1,
   scrollbar: {
     el: '.swiper-scrollbar',
     hide: true,
@@ -36,13 +117,10 @@ const swiper2 = new Swiper('.swiper2', {
   speed: 750,
   keyboard: {
     enabled: true,
-    onlyInViewport: true,
-
   },
   grabCursor: true,
   simulateTouch: true,
 });
-
 
 function handleResize() {
   swiper2.params.slidesPerView = countSlidesPerView();
@@ -53,7 +131,9 @@ window.addEventListener('resize', handleResize);
 
 async function fetchReviews() {
   try {
-    const response = await fetch("https://portfolio-js.b.goit.study/api/reviews");
+    const response = await fetch(
+      'https://portfolio-js.b.goit.study/api/reviews'
+    );
     if (!response.ok) {
       throw new Error(`Error ${response.status}`);
     }
@@ -70,7 +150,7 @@ async function fetchReviews() {
   }
 }
 
-const renderReviews = (data) => {
+const renderReviews = data => {
   data.forEach(element => {
     const markup = `<li class="reviews-item swiper-slide">
       <img class="img-reviews" src="${element.avatar_url}" alt="reviews" loading="lazy" width="48" height="48"/>
@@ -79,6 +159,6 @@ const renderReviews = (data) => {
     </li>`;
     reviemList.insertAdjacentHTML('beforeend', markup);
   });
-}
+};
 
 fetchReviews();
